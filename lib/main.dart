f09ec869bf0ac49c5094886b86a03970cf96f241
Admin <Admin@DESKTOP-9MILIL3>
2022-04-04 15:07:03 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:baitapbuoi6/product_list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Lession06',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductList(),
+    );
+  }
+}
+
+
